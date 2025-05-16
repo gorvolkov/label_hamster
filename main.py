e6a26a391cc646449a3453d77d4dd2c146da9144
gorvolkov <steppenwolfff@mail.ru>
2025-05-16 12:22:36 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+print("Welcome to Label Hamster")
+
+
+# from previous
+from dataclasses import dataclass
+# import pandas as pd
+
+# book_1 = "xslx_files/Fleor для загрузки в базу  ШК.xlsx"
+# sheet_1 = "ШК"
+#
+# @dataclass
+# class MusicInstrument:
+#     id: int
+#     name: str
+#     description: str
+#     ean: int
+#
+#
+# def read_from_xlsx(book: str, sheet: str) -> list[str]:
+#
+#     df = pd.read_excel(book, sheet_name=sheet)
+#
+#     # Выведите все строки построчно
+#     for index, row in df.iterrows():
+#         print(f"Строка {index + 1}: {row.tolist()}")
+#
+#     for index, row in df.iterrows():
+#         print(f"Строка {index + 1}: Column1 = {row['Column1']}, Column2 = {row['Column2']}")
+#
+#
+# def get_barcode(ean: int) -> None:
+#     ...
+
+
+
+# if __name__ == "__main__":
+#     read_from_xlsx(book_1, sheet_1)
